e02f0497356faee64a62710ac9aae321dd0d836a
ceciliazaragoza <czarago3@lion.lmu.edu>
2025-02-20 09:35:33 -0800

    imported ohm-js and graph-stringify, updated package.json to have author names and github repo, added breaketh statement and FuncCall

---
+++ src/parser.js
@@ -0,0 +1,12 @@
+import * as fs from "fs";
+import * as ohm from "ohm-js";
+
+const grammar = ohm.grammar(fs.readFileSync("src/yeoldedragon.ohm", "utf8"));
+
+export default function parse(sourceCode) {
+  const match = grammar.match(sourceCode);
+  if (match.failed()) {
+    throw match.message;
+  }
+  return match;
+}
