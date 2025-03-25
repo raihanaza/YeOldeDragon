7cc8999eef755d83856ff9d3bf06e346570746df
ceciliazaragoza <czarago3@lion.lmu.edu>
2025-03-25 08:44:34 -0700

    can now run tests correctly without timing out from parent

--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -1,37 +1,41 @@
 import { describe, it } from "node:test";
-import assert from "node:assert/strict";
+import { ok, deepEqual, throws } from "node:assert/strict";
 import parse from "../src/parser.js";
 import analyze from "../src/analyzer.js";
 import { program, variableDeclaration, variable, intType, floatType, binaryExpression } from "../src/core.js";
 
 const semanticChecks = [
   ["int variable declaration", "thine x: int = 1;"],
-  //["string variable declaration", "thine x: float = 1.0;"],
+  ["string variable declaration", "thine x: float = 1.0;"],
 ];
 
 const semanticErrors = [
-  //  ["variable declaration with mismatched types", "thine x: bool = 1;", /Type mismatch. Expected type bool but got int/],
-  ["variable declaration with mismatched types", "thine x: bool = 1;"],
+  //["variable declaration with mismatched types", "thine x: bool = 1;", /Type mismatch. Expected type bool but got int/],
+  //["variable declaration with mismatched types", "thine x: bool = 1;"],
 ];
 
 describe("The analyzer", () => {
   for (const [scenario, source] of semanticChecks) {
     it(`recognizes ${scenario}`, () => {
-      assert.ok(analyze(parse(source)));
+      ok(analyze(parse(source)));
     });
   }
 
   for (const [scenario, source, errorMessagePattern] of semanticErrors) {
     it(`throws on ${scenario}`, () => {
-      assert.throws(() => analyze(parse(source)), errorMessagePattern);
+      throws(() => analyze(parse(source)), errorMessagePattern);
     });
   }
 
-  it("produces the expected representation for a trivial program", () => {
-    for (const [scenario, source] of semanticChecks) {
-      it(`recognizes ${scenario}`, () => {
-        assert.ok(analyze(parse(source)));
-      });
-    }
-  });
+  //   it("produces the expected representation for a trivial program", () => {
+  //     deepEqual(
+  //       analyze(parse("let x = π + 2.2;")),
+  //       program([
+  //         variableDeclaration(
+  //           variable("x", true, floatType),
+  //           binary("+", variable("π", false, floatType), 2.2, floatType)
+  //         ),
+  //       ])
+  //     );
+  //   });
 });
