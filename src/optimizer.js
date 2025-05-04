e2972fcd432a7a010f02c7aed309ed5aac204b17
laurenindira <50187316+laurenindira@users.noreply.github.com>
2025-05-04 14:47:53 -0700

    beginning of optimizer, 74% tests passing

--- src/optimizer.js
+++ src/optimizer.js
@@ -1,3 +1,246 @@
+import * as core from "./core.js";
+
 export default function optimize(node) {
-  return node;
+  return optimizers?.[node.kind]?.(node) ?? node
+}
+
+const isZero = n => n === 0 | n === 0.0 | n === 0n
+const isOne = n => n === 1 | n === 1.0 | n === 1n
+
+const optimizers = {
+  Program(p) {
+    p.statements = p.statements.flatMap(optimize)
+    return p
+  },
+  VariableDeclaration(d) {
+    d.variable = optimize(d.variable)
+    d.initializer = optimize(d.initializer)
+    return d
+  },
+  ClassDeclaration(d) {
+    //d.type = optimize(d.type)
+    //d.fields = d.fields.map(optimize)
+    if (d.type.methods) {
+      d.type.methods = d.type.methods.map(optimize)
+    }
+    return d
+  },
+  ObjectCall(c) {
+    c.callee = optimize(c.callee)
+    c.args = c.args.map(optimize)
+    return c
+  },
+  ObjectType(t) {
+    t.fields = t.fields.map(optimize)
+    if (t.methods) {
+      t.methods = t.methods.map(optimize)
+    }
+    return t
+  },
+  Argument(a) {
+    return a
+  },
+  FieldArgument(a) {
+    return a
+  },
+  Field(f) {
+    return f
+  },
+  IncrementStatement(s) {
+    s.variable = optimize(s.variable)
+    return s
+  }, 
+  DecrementStatement(s) {
+    s.variable = optimize(s.variable)
+    return s
+  },
+  AssignmentStatement(s) {
+    s.target = optimize(s.target)
+    s.source = optimize(s.source)
+    if (s.source === s.target) { 
+      return []
+    }
+    return s
+  },
+  BreakStatement(s) {
+    return s
+  },
+  ReturnStatement(s) {
+    s.expression = optimize(s.expression)
+    return s
+  },
+  ShortReturnStatement(s) {
+    return s
+  },
+  UnaryExpression(e) {
+    e.op = optimize(e.op)
+    e.operand = optimize(e.operand)
+    if (e.operand.constructor === Number) {
+      if (e.op === "-") return -e.operand
+    }
+    return e
+  },
+  BinaryExpresion(e) {
+    e.op = optimize(e.op)
+    e.left = optimize(e.left)
+    e.right = optimize(e.right)
+    if (e.op === "??") {
+      if (e.left.kind === "EmptyOptional") {
+        return e.right
+      }
+    } else if (e.op === "&&") {
+      if (e.left === true) return e.right
+      if (e.right === true) return e.left
+    } else if (e.op === "||") {
+      if (e.left === false) return e.right
+      if (e.right === false) return e.left
+    } else if ([Number, BigInt].includes(e.left.constructor)) {
+      if ([Number, BigInt].includes(e.right.constructor)) {
+        if (e.op === "+") return e.left + e.right
+        if (e.op === "-") return e.left - e.right
+        if (e.op === "*") return e.left * e.right
+        if (e.op === "/") return e.left / e.right
+        if (e.op === "**") return e.left ** e.right
+        if (e.op === "<") return e.left < e.right
+        if (e.op === "<=") return e.left <= e.right
+        if (e.op === ">") return e.left > e.right
+        if (e.op === ">=") return e.left >= e.right
+        if (e.op === "==") return e.left === e.right
+        if (e.op === "!=") return e.left !== e.right
+      }
+      if (isZero(e.left) && e.op === "+") return e.right
+      if (isZero(e.left) && e.op === "-") return core.unaryExpression("-", e.right, e.right.type)
+      if (isOne(e.left) && e.op === "*") return e.right
+      if (isZero(e.left) && e.op === "*") return e.left
+      if (isZero(e.left) && ["*", "/"].includes(e.op)) return e.left
+    } else if ([Number, BigInt]. includes(e.right.constructor)) {
+      if (["+", "-"].includes(e.op) && isZero(e.right)) return e.left
+      if (["*", "/"].includes(e.op) && isOne(e.right)) return e.left
+      if (e.op === "*" && isZero(e.right)) return e.right
+      if (e.op === "**" && isZero(e.right)) return 1
+    }
+    return e
+  },
+  TernaryExpression(e) {
+    e.op = optimize(e.op)
+    e.consequence = optimize(e.consequence)
+    e.alternate = optimize(e.alternate)
+    if (e.op.constructor === Boolean) {
+      return e.op ? e.consequence : e.alternate
+    }
+    return e
+  },
+  NilCoalescingExpression(e) {
+    e.op = optimize(e.op)
+    e.left = optimize(e.left)
+    e.right = optimize(e.right)
+    if (e.left.kind === "EmptyOptional") {
+      return e.right
+    }
+    return e
+  },
+  IfStatement(s) {
+    s.condition = optimize(s.condition)
+    s.consequence = s.consequence.flatMap(optimize)
+    //s.alternate = s.alternate.flatMap(optimize)
+    // if (s.alernate?.kind?.endsWith?.("IfStatement")) {
+    //   s.alternate = optimize(s.alternate)
+    // } else {
+    //   s.alternate = s.alternate.flatMap(optimize)
+    // }
+    // if (s.condition.constructor === Boolean) {
+    //   return s.test ? s.consequence : s.alternate
+    // }
+    return s
+  },
+  ShortIfStatement(s) {
+    s.condition = optimize(s.condition)
+    s.consequence = s.consequence.flatMap(optimize)
+    if (s.condition.constructor === Boolean) {
+      return s.test ? s.consequence : s.alternate
+    }
+    return s
+  },
+  LoopStatement(s) {
+    s.condition = optimize(s.condition)
+    if (s.condition === false) {
+      return []
+    }
+    s.body = s.body.flatMap(optimize)
+    return s
+  },
+  RepeatStatement(s) {
+    s.count = optimize(s.count)
+    if (s.count === 0) {
+      return []
+    }
+    s.body = s.body.flatMap(optimize)
+    return s
+  },
+  ForEachStatement(s) {
+    s.iterator = optimize(s.iterator)
+    s.collection = optimize(s.collection)
+    s.body = s.body.flatMap(optimize)
+    if (s.collection?.kind === "EmptyList") {
+      return []
+    } 
+    return s
+  },
+  ForRangeStatement(s) {
+    s.iterator = optimize(s.iterator)
+    s.start = optimize(s.start)
+    s.op = optimize(s.op)
+    s.end = optimize(s.end)
+    s.body = s.body.flatMap(optimize)
+    if (s.start.constructor === Number && s.end.constructor === Number && s.start > s.end) {
+      return []
+    }
+    return s
+  },
+  EmptyOptional(o) {
+    return o
+  },
+  ListExpression(e) {
+    e.elements = e.elements.map(optimize)
+    return e
+  },
+  EmptyListExpression(e) {
+    return e
+  },
+  SubscriptExpression(e) {
+    e.list = optimize(e.list)
+    e.index = optimize(e.index)
+    return e
+  },
+  MemberExpression(e) {
+    // e.object = optimize(e.object)
+    return e
+  },
+
+  FunctionDeclaration(d) {
+    // d.func = optimize(d.func)
+    return d
+  },
+  FunctionType(f) {
+    // f.params = f.params.map(optimize)
+    // f.returnType = optimize(f.returnType)
+    return f
+  },
+  FunctionCall(c) {
+    // c.callee = optimize(c.callee)
+    // c.args = c.args.map(optimize)
+    return c
+  },
+  Function(f) {
+    if (f.body) {
+      f.body = f.body.flatMap(optimize)
+    }
+  },
+  PrintStatement(s) {
+    s.expressions = s.expressions.map(optimize)
+    return s
+  },
+  StringExpression(e) {
+    return e
+  },
 }
--- test/optimizer.test.js
+++ test/optimizer.test.js
@@ -1,31 +1,95 @@
-// import { describe, it } from "node:test"
-// import assert from "node:assert/strict"
-// import optimize from "../serc/optimizer.js"
-// import * as core from "../src/core.js"
+import { describe, it } from "node:test"
+import assert from "node:assert/strict"
+import optimize from "../src/optimizer.js"
+import * as core from "../src/core.js"
 
-// // Basic variables used in test cases
-// const x = core.variable("x", core.intType, true)
-// const x_list = core.variable("x_list", core.listType(core.intType), true)
-// const x_inc = core.incrementStatement(x_inc)
-// const x_dec = core.decrementStatement(x_dec)
-// const return_add = core.returnStatement(core.binaryExpression("+", 1, 1, core.intType))
-// const return_string = core.returnStatement("Hello, World!")
-// const add_operation = core.binaryExpression("+", 1, 1, core.intType)
-// const assign = (v, e) => core.assignmentStatement(v, e, core.intType)
-// const empty_list = core.emptyListExpression(core.intType)
-// const program = core.program
+// Basic variables used in test cases
+const x_int = core.variable("x", core.intType, true)
+const x_float = core.variable("x_float", core.floatType, true)
+const x_list = core.variable("x_list", core.listType(core.intType), true)
+const x_inc = core.incrementStatement(x_int)
+const x_dec = core.decrementStatement(x_int)
+const return_add = core.returnStatement(core.binaryExpression("+", 1, 1, core.intType))
+const return_string = core.returnStatement("Hello, World!")
+const add_operation = core.binaryExpression("+", 1, 1, core.intType)
+const assign = (v, e) => core.assignmentStatement(v, e, core.intType)
+const empty_list = core.emptyListExpression(core.intType)
+const and = (...c) => c.reduce((a, b) => core.binaryExpression("&&", a, b, core.boolType))
+const or = (...d) => d.reduce((a, b) => core.binaryExpression("||", a, b, core.boolType))
+const not = c => core.unaryExpression("ne", c, core.boolType)
+const neg_int = x => core.unaryExpression("-", x, core.intType)
+const list_statement = (...elements) => core.listExpression(elements, core.anyType)
+const sub = (l, i) => core.subscriptExpression(l, i)
+const less = (x, y) => core.binaryExpression("<", x, y, core.boolType)
+const eq = (x, y) => core.binaryExpression("==", x, y, core.boolType)
+const program = core.program
 
-// const tests = [
-//     ["folds +", core.binaryExpression("+", 2, 3, core.intType), 5],
-//     ["folds -", core.binaryExpression("-", 5, 3, core.intType), 2],
-//     ["folds *", core.binaryExpression("*", 2, 10, core.intType), 20],
-//     ["folds /", core.binaryExpression("/", 10, 2, core.intType), 5],
-// ]
+const tests = [
+    ["folds +", core.binaryExpression("+", 2, 3, core.intType), 5],
+    ["folds -", core.binaryExpression("-", 5, 3, core.intType), 2],
+    ["folds *", core.binaryExpression("*", 2, 10, core.intType), 20],
+    ["folds /", core.binaryExpression("/", 10, 2, core.intType), 5],
+    ["folds %", core.binaryExpression("%", 10, 3, core.intType), 1],
+    ["folds **", core.binaryExpression("**", 2, 3, core.intType), 8],
+    ["folds <", core.binaryExpression("<", 2, 3, core.boolType), true],
+    ["folds <=", core.binaryExpression("<=", 2, 2, core.boolType), true],
+    ["folds >", core.binaryExpression(">", 3, 2, core.boolType), true],
+    ["folds >=", core.binaryExpression(">=", 2, 2, core.boolType), true],
+    ["folds ==", core.binaryExpression("==", 2, 2, core.boolType), true],
+    ["folds !=", core.binaryExpression("!=", 2, 3, core.boolType), true],
+    
+    ["optimizes +0 for ints", core.binaryExpression("+", 5, 0, core.intType), 5],
+    ["optimizes -0 for ints", core.binaryExpression("-", 5, 0, core.intType), 5],
+    ["optimizes 0+ for floats", core.binaryExpression("+", 0.0, 2.4, core.floatType), 2.4],
+    ["optimizes 0- for floats", core.binaryExpression("-", 0.0, 2.4, core.floatType), -2.4],
+    ["optimizes 1* for ints", core.binaryExpression("*", 1, 2, core.intType), 2],
+    ["optimizes /1 for ints", core.binaryExpression("/", 3, 1, core.intType), 3],
+    ["optimizes *1 for ints", core.binaryExpression("*", 3, 1, core.intType), 3],
+    ["optimizes *1.0 for floats", core.binaryExpression("*", 3.0, 1.1, core.floatType), 3.0],
+    ["optimizes +0 for floats", core.binaryExpression("+", 5.0, 0.0, core.floatType), 5.0],
+    ["optimizes -0 for floats", core.binaryExpression("-", 5.0, 0.0, core.floatType), 5.0],
+    ["optimizes 0/", core.binaryExpression("/", 0, 1, core.intType), 0],
+    ["optimizes 0.0/", core.binaryExpression("/", 0.0, 1.1, core.floatType), 0.0],
+    ["folds negation", core.unaryExpression("-", 5, core.intType), -5],
+    ["optimizes 1** for ints", core.binaryExpression("**", 1, 2, core.intType), 1],
+    ["optimizes 1** for floats", core.binaryExpression("**", 1.0, 2.0, core.floatType), 1.0],
+    ["optimizes **0", core.binaryExpression("**", 2, 0, core.intType), 1],
+    ["removes left false from ||", or(false, less(1, 2)), less(1, 2)],
+    ["removes right false from ||", or(less(1, 2), false), less(1, 2)],
+    ["removes left true from &&", and(true, less(1, 2)), less(1, 2)],
+    ["removes right true from &&", and(less(1, 2), true), less(1, 2)],
+    ["folds and", and(true, true), true],
+    ["folds or", or(true, false), true],
+    ["folds and", and(false, false), false],
+    ["folds or", or(false, false), false],
+    ["removes x=x at beginning", program([assign(x_int, x_int), x_inc]), program([x_inc])],
+    ["removes x=x at end", program([x_inc, assign(x_int, x_int)]), program([x_inc])],
+    ["removes x=x in middle", program([x_inc, assign(x_int, x_int), x_inc]), program([x_inc, x_inc])],
+    ["optimizes if true", core.ifStatement(true, [x_inc], []), [x_inc]],
+    ["optimizes if false", core.ifStatement(false, [], [x_inc]), [x_inc]],
+    ["optimizes short if true", core.shortIfStatement(true, [x_inc]), [x_inc]],
+    ["optimizes short if false", core.shortIfStatement(false, [x_inc]), []],
+    ["optimizes while false", program([core.whileStatement(false, [x_inc])]), program([])],
+    ["optimizes repeat 0", program([core.repeatStatement(0, [x_inc])]), program([])],
+    ["optimizes for range", core.forRangeStatement(x_int, 0, "...", 5, [x_inc]), []],
+    ["optimizes left conditional true", core.ternaryExpression("?", true, 54, 234), 54],
+    ["optimizes left conditional false", core.ternaryExpression("?", false, 54, 234), 234],
+    ["optimizes in subscript", sub(x_list, add_operation), sub(x_list, 2)],
+    ["optimizes in list literals", list_statement(add_operation, 4, 2), list_statement(2, 4, 2)],
+    ["optimizes in arguments", core.functionCall("foo", [add_operation], core.intType), core.functionCall("foo", [2], core.intType)],
+    // ["passes through non-optimizable constructs",
+    //     ...Array(2).fill([
+    //         core.program([core.shortReturnStatement]),
+    //         core.variableDeclaration("x", 5, core.intType),
+    //         core.assignmentStatement("x", 5, core.intType),
+    //     ])
+    // ]
+]
 
-// describe("The optimizer", () => {
-//     for (const [scenario, before, after] of tests) {
-//         it(`${scenario}`, () => {
-//             assert.deepEqual(optimize(before), after)
-//         })
-//     }
-// })
+describe("The optimizer", () => {
+    for (const [scenario, before, after] of tests) {
+        it(`${scenario}`, () => {
+            assert.deepEqual(optimize(before), after)
+        })
+    }
+})
