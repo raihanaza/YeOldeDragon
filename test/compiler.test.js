0f30fd950df16d3a9f1da20d511be6946cdf217b
laurenindira <50187316+laurenindira@users.noreply.github.com>
2025-05-09 15:44:32 -0700

    adding compiler test, not sure if totally needed but just in case

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,8 +1,46 @@
 import { describe, it } from 'node:test';
-import { deepEqual } from 'node:assert/strict';
+import assert from 'node:assert/strict';
+import compile from "../src/compiler.js";
 
-describe ('Compiler', () => {
-    it('is alive', () => {
-        deepEqual(1, 1);
-    });
+const sampleProgram = [
+    [
+    `
+    thine x: string = "hello";
+    proclaim("Good morrow, \${x} fair world!");
+
+    don factorial(num: int) -> int {
+        perchance ((num == 0) || (num == 1)) {
+            return 1;
+        }
+        return num * factorial(num: num - 1);
+    }
+
+    proclaim(factorial(num: 5));
+    `
+    ],
+]
+    
+describe ('The compiler', () => {
+    it("throws when the output type is missing", () => {
+        assert.throws(() => compile(sampleProgram), /Unknown output type/)
+    })
+    it ("throws when the output type is unknown", () => {
+        assert.throws(() => compile(sampleProgram, "no such type"), /Unknown output type/)
+    })
+    it ("accepts the parsed option", () => {
+        const compiled = compile(sampleProgram, "parsed");
+        assert(compiled.startsWith("Syntax is ok"))
+    })
+    it ("accepts the analyzed option", () => {
+        const compiled = compile(sampleProgram, "analyzed");
+        assert(compiled.kind === "Program")
+    })
+    it ("accepts the optimized option", () => {
+        const compiled = compile(sampleProgram, "optimized");
+        assert(compiled.kind === "Program")
+    })
+    it ("generates js code when given the js option", () => {
+        const compiled = compile(sampleProgram, "js");
+        assert(compiled.startsWith("let x_1 = \"hello\";"))
+    })
 });
